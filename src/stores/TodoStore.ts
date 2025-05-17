eef03542e07de51d218d534eaba0a841480e7f11
TeoVincent Artur Wincenciak <artur.wincenciak@gmail.com>
2025-05-17 13:39:35 +0200

    Add TodoStore with basic todo management actions
    Introduces a MobX-based TodoStore for managing a todo list. Includes functionalities to add, toggle, and remove todo items, with each task uniquely identified by an auto-incrementing ID. This forms the foundation for managing todos in the application.

---
+++ src/stores/TodoStore.ts
@@ -0,0 +1,35 @@
+import { makeAutoObservable } from 'mobx';
+
+export interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+export class TodoStore {
+  todos: Todo[] = [];
+  nextId = 1;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  addTodo(title: string) {
+    this.todos.push({
+      id: this.nextId++,
+      title,
+      done: false,
+    });
+  }
+
+  toggleTodo(id: number) {
+    const todo = this.todos.find((t) => t.id == id);
+    if (todo) {
+      todo.done = !todo.done;
+    }
+  }
+
+  removeTodo(id: number) {
+    this.todos = this.todos.filter((t) => t.id != id);
+  }
+}
