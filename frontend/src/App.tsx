f0e7ae968c092c916e520ca73ca29c31c215a260
TeoVincent Artur Wincenciak <artur.wincenciak@gmail.com>
2025-05-18 17:49:13 +0200

    Add useEffect to load todos on app initialization
    This ensures todos are fetched from the store when the app starts. Added error handling and a log statement to track loading success or issues.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,21 @@ import { Box, CircularProgress, Container, Typography } from '@mui/material';
 import AddTodoFrom from './components/AddTodoFrom.tsx';
 import TodoList from './components/TodoList.tsx';
 import { todoStore } from './stores/TodoStore.ts';
+import {useEffect} from "react";
 
 const App = observer(() => {
+  useEffect(() => {
+    const load = async () => {
+      try {
+        await todoStore.loadTodos();
+      } catch (err) {
+        console.error('Error loading tasks', err);
+      }
+    };
+
+    load().then(r => console.log("Done loading tasks", r));
+  }, []);
+
   return (
     <Container maxWidth="sm">
       <Box mt={4}>
