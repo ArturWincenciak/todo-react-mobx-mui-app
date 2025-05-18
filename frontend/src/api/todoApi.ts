0888f3350fb0a32006f736c3f8e8ebf2788c8f53
TeoVincent Artur Wincenciak <artur.wincenciak@gmail.com>
2025-05-18 13:04:16 +0200

    Add todoApi.ts with the fetchTodos method
    Introduce a new file `todoApi.ts` to handle API interactions for fetching todos. It includes the `fetchTodos` function and sets up Axios with a base URL and JSON headers. This serves as the initial setup for managing todo-related API requests.

---
+++ frontend/src/api/todoApi.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export interface TodoDto {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+const API = axios.create({
+  baseURL: "http://localhost:5128/api/todos",
+  headers: {
+    'Content-Type': 'application/json'
+  },
+});
+
+export const fetchTodos = async () => {
+  const reset = await API.get<TodoDto[]>('/');
+  return reset.data;
+};
